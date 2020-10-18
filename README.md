d2e65af6ca63f6aacbc56b65b4e7e6b62e6e0c13
S11as <50956430+S11as@users.noreply.github.com>
2020-10-18 14:10:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# mp2-lab2
+[![Build Status](https://travis-ci.com/S11as/mp2-lab2.svg?branch=main)](https://travis-ci.com/S11as/mp2-lab2)
---
+++ test/test_boilerplate.cpp
@@ -0,0 +1,9 @@
+#include "MyVector.h"
+
+#include <gtest.h>
+
+TEST(TestSuite, can_build_and_run_sample_test)
+{
+    EXPECT_EQ(true, true);
+}
+
